06eab4b92bd98453e3f703b0991c103d9c4d252d
euhansarkar <euhansarkar@gmail.com>
2022-11-26 23:41:23 +0600

    product delete api created

--- index.js
+++ index.js
@@ -178,6 +178,13 @@ async function run() {
       return res.send(result);
     })
 
+    app.delete(`/products/:id`, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const product = await productsCollection.findOne(query);
+      return res.send(product);
+    })
+
 
   } finally {
   }
